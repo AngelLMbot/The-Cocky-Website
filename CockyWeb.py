12187a69a5cb4526cee7b89e9d7f1933c4fd53ea
AngelLM <alm37454@gmail.com>
2017-08-09 23:20:33 +0200

    Automatic commit 2017-08-09 21:20:33

--- CockyWeb.py
+++ CockyWeb.py
@@ -21,38 +21,38 @@ TWuser = '_AngelLM'                                     # Twitter username. http
 GGname = 'thor-opensource-3d-printable-robotic-arm'     # Google Group name. https://groups.google.com/forum/#!forum/GROUPNAME
 IGuser = 'angel_lm_'                                    # Instagram username. https://www.instagram.com/
 
-#
-# separator=''
-#
-# Fbase = open("indexbase.txt", "r")
-# Textbase = Fbase.read()
-# Fbase.close()
-#
-# Fsplitted = Textbase.split(":")
-#
-# GHstats = github.getStats(GHuser)
-# TVstats = thingiverse.getStats(TVuser)
-# YTstats = youtube.getStats(YTuser)
-# HDstats = hackaday.getStats(HDuser)
-# TWstats = twitter.getStats(TWuser)
-# GGstats = googlegroups.getStats(GGname)
-# IGstats = instagram.getStats(IGuser)
+
+separator=''
+
+Fbase = open("indexbase.txt", "r")
+Textbase = Fbase.read()
+Fbase.close()
+
+Fsplitted = Textbase.split(":")
+
+GHstats = github.getStats(GHuser)
+TVstats = thingiverse.getStats(TVuser)
+YTstats = youtube.getStats(YTuser)
+HDstats = hackaday.getStats(HDuser)
+TWstats = twitter.getStats(TWuser)
+GGstats = googlegroups.getStats(GGname)
+IGstats = instagram.getStats(IGuser)
 #
 dateNow = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-# StatsData = [GHstats[0], GHstats[1], GHstats[2], GHstats[3], GHstats[4], TVstats[0], TVstats[1], TVstats[2], TVstats[3], TVstats[4], TVstats[5], TVstats[6], YTstats[0], YTstats[1], YTstats[2], YTstats[3], HDstats[0], HDstats[1], HDstats[2], HDstats[3], HDstats[4], TWstats, GGstats[0], GGstats[1], IGstats[0], IGstats[1], IGstats[2], IGstats[3]]
-#
-# for s in range(len(Fsplitted)-2):
-#     Fsplitted[s]=Fsplitted[s]+': '+str(StatsData[s])+'</p>'
-#
-# Fsplitted[len(Fsplitted)-2]=Fsplitted[len(Fsplitted)-2]+': '+dateNow+'</p>'
-#
-# Fjoined = separator.join(Fsplitted)
-#
-#
-# Ffinal = open("index.html", "w")
-# Ffinal.write(Fjoined)
-# Ffinal.close()
+StatsData = [GHstats[0], GHstats[1], GHstats[2], GHstats[3], GHstats[4], TVstats[0], TVstats[1], TVstats[2], TVstats[3], TVstats[4], TVstats[5], TVstats[6], YTstats[0], YTstats[1], YTstats[2], YTstats[3], HDstats[0], HDstats[1], HDstats[2], HDstats[3], HDstats[4], TWstats, GGstats[0], GGstats[1], IGstats[0], IGstats[1], IGstats[2], IGstats[3]]
+
+for s in range(len(Fsplitted)-2):
+    Fsplitted[s]=Fsplitted[s]+': '+str(StatsData[s])+'</p>'
+
+Fsplitted[len(Fsplitted)-2]=Fsplitted[len(Fsplitted)-2]+': '+dateNow+'</p>'
+
+Fjoined = separator.join(Fsplitted)
+
+
+Ffinal = open("index.html", "w")
+Ffinal.write(Fjoined)
+Ffinal.close()
 
 call(["git", "add", "./"])
 call(["git", "commit", "-m", "Automatic commit "+dateNow])
-# call(["git", "push", "origin", "master"])
+call(["git", "push", "origin", "master"])
