4aea661f115160241568d9765f57ce3fe72956b8
AngelLM <alm37454@gmail.com>
2017-08-09 23:08:22 +0200

    Automatic commit

--- CockyWeb.py
+++ CockyWeb.py
@@ -9,6 +9,7 @@ import googlegroups
 import instagram
 import re
 from subprocess import call
+from time import gmtime, strftime
 
 # Users & Names variables. Fill them with your usernames if you want the stats of that platform. Leave it blank if you don't want the stats.
 
@@ -36,10 +37,13 @@ HDstats = hackaday.getStats(HDuser)
 TWstats = twitter.getStats(TWuser)
 GGstats = googlegroups.getStats(GGname)
 IGstats = instagram.getStats(IGuser)
+
+dateNow = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 StatsData = [GHstats[0], GHstats[1], GHstats[2], GHstats[3], GHstats[4], TVstats[0], TVstats[1], TVstats[2], TVstats[3], TVstats[4], TVstats[5], TVstats[6], YTstats[0], YTstats[1], YTstats[2], YTstats[3], HDstats[0], HDstats[1], HDstats[2], HDstats[3], HDstats[4], TWstats, GGstats[0], GGstats[1], IGstats[0], IGstats[1], IGstats[2], IGstats[3]]
 
-for s in range(len(Fsplitted)-1):
+for s in range(len(Fsplitted)-2):
     Fsplitted[s]=Fsplitted[s]+': '+str(StatsData[s])+'</p>'
+Fsplitted[len(Fsplitted)-1]=Fsplitted[len(Fsplitted)-1]+dateNow+'</p>'
 
 Fjoined = separator.join(Fsplitted)
 
@@ -49,5 +53,5 @@ Ffinal.write(Fjoined)
 Ffinal.close()
 
 call(["git", "add", "./"])
-call(["git", "commit", "-m", "\"Automatic commit\""])
+call(["git", "commit", "-m", "Automatic commit"])
 call(["git", "push", "origin", "master"])
